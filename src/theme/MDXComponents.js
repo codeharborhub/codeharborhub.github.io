eae3837ba5c777a48687e0a518502d5df1bfa336
Ajay Dhangar <ajaydhangar49@gmail.com>
2026-09-25 09:12:54 +0530

    done html getting started docs

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -19,7 +19,6 @@ import Admonition from '@theme/Admonition';
 import TOCInline from '@theme/TOCInline';
 import CodePenEmbed from '@site/src/components/CodePenEmbed';
 import CodePreview from '@site/src/components/CodePreview';
-import { Timeline, TimelineEvent } from '@site/src/components/Timeline';
 
 export default {
   // Re-use the default mapping
@@ -43,6 +42,4 @@ export default {
   TOCInline,
   CodePenEmbed,
   CodePreview,
-  Timeline, 
-  TimelineEvent,
 };
